537fe4b84d599a87f3cafb44f70f05d5fc17404c
BigMoneyBones <87784559+BigMoneyBones@users.noreply.github.com>
2022-07-07 10:16:07 -0500

    update

--- routes/blogs.js
+++ routes/blogs.js
@@ -10,7 +10,7 @@ router.get("/all-blogs", async (req, res) => {
   try {
     const collection = await blogsDB().collection("blogs50");
     const posts = await collection.find({}).toArray();
-    res.json(posts);
+    res.send({ message: posts });
   } catch (error) {
     res.status(500).send("error fetching posts " + error);
   }
