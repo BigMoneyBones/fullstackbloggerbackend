b9bc41a5c19c0f960bbf6478b671e0ad2a9a46ad
BigMoneyBones <87784559+BigMoneyBones@users.noreply.github.com>
2022-07-09 11:03:40 -0500

    fixed 500 error bug

--- routes/blogs.js
+++ routes/blogs.js
@@ -1,22 +1,21 @@
 var express = require("express");
-const { blogsDB } = require("../mongo");
 var router = express.Router();
+const { blogsDB } = require("../mongo");
 
 router.get("/hello-blogs", (req, res) => {
   res.json({ message: "Hello from express!" });
 });
 
 router.get("/all-blogs", async (req, res) => {
-  const limit = Number(req.query.limit);
-  const skip = Number(req.query.limit) * (Number(req.query.page) - 1);
-  const sortField = req.query.sortField;
-  // if sort field is equal to "ASC", then sequential (1), if not, sequential in reverse order (-1).
-  const sortOrder = req.query.sortOrder === "ASC" ? 1 : -1;
-  const filterField = req.query.filterField;
-  const filterValue = req.query.filterValue;
-
   try {
     const collection = await blogsDB().collection("blogs50");
+    const limit = Number(req.query.limit);
+    const skip = Number(req.query.limit) * (Number(req.query.page) - 1);
+    const sortField = req.query.sortField;
+    // if sort field is equal to "ASC", then sequential (1), if not, sequential in reverse order (-1).
+    const sortOrder = req.query.sortOrder === "ASC" ? 1 : -1;
+    const filterField = req.query.filterField;
+    const filterValue = req.query.filterValue;
 
     let filterObj = {};
     if (filterField && filterValue) {
@@ -34,10 +33,42 @@ router.get("/all-blogs", async (req, res) => {
       .limit(limit)
       .skip(skip)
       .toArray();
-
     res.json({ message: posts });
   } catch (error) {
     res.status(500).send("error fetching posts " + error);
+  }
+});
+
+router.post("/blog-submit", async (req, res, next) => {
+  try {
+    // const collection = await blogsDB().collection("blogs50");
+    // const sortedBlogArray = await collection.find({}).sort({ id: 1 }).toArray();
+    // const lastBlog = sortedBlogArray[sortedBlogArray.length - 1];
+
+    const collection = await blogsDB().collection("blogs50");
+    const sortedBlogArray = await collection.find({}).sort({ id: 1 }).toArray();
+    const lastBlog = sortedBlogArray[sortedBlogArray.length - 1];
+
+    console.log(req.body);
+    const title = req.body.title;
+    const text = req.body.text;
+    const author = req.body.author;
+    const category = req.body.category;
+
+    const blogPost = {
+      title: title,
+      text: text,
+      author: author,
+      category: category,
+      createdAt: new Date(),
+      id: Number(lastBlog.id + 1),
+      lastModified: new Date(),
+    };
+
+    await collection.insertOne(blogPost);
+    // res.status(200).send("Post has been submitted");
+  } catch (error) {
+    res.status(500).send("Error posting blog." + error);
   }
 });
 
